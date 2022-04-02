8450289f173b3405661038fcee8806adf39431e1
SandyHoey <101828089+SandyHoey@users.noreply.github.com>
2022-04-02 09:50:08 -0600

    Delete TimeBetweenCow.R
    placed in wrong location

--- TimeBetweenCow.R
+++
@@ -1,9 +0,0 @@
-#'  Cameron Ho
-#'  Sarah Bassing Quantitative Ecology Lab
-#'  Creates data table that shows the time between a cow detection and a prey species detection
-#'  Requires the functions from the 'Covariate Functions' Script
-
-  
-#'  packages
-library(data.table)
-library(tidyverse)
