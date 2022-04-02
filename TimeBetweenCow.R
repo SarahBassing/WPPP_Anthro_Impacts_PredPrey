77467015a4a7e5b2bb91dac0ca2e0b56bb955961
sandyhoey <ho.cameron98@gmail.com>
2022-04-02 09:46:58 -0600

    First crack at script calculating time since cow detection

---
+++ TimeBetweenCow.R
@@ -0,0 +1,9 @@
+#'  Cameron Ho
+#'  Sarah Bassing Quantitative Ecology Lab
+#'  Creates data table that shows the time between a cow detection and a prey species detection
+#'  Requires the functions from the 'Covariate Functions' Script
+
+  
+#'  packages
+library(data.table)
+library(tidyverse)
