0c4a510b7e77918ee5603f056d2e0c29fc9250a9
SarahBassing <sarah.bassing@gmail.com>
2022-04-18 16:14:21 -0700

    Added argument to change time interval for deteciton events, updated column names & notes

--- Scripts/Hunter.R
+++ Scripts/Hunter.R
@@ -47,36 +47,42 @@
   week_hunt_cov1 <-  do.call(rbind, cov1(hunter_data, "weeks", "human"))
   day_hunt_cov1 <- do.call(rbind, cov1(hunter_data, "days", "human"))
   
-  week_hunt_cov2 <- do.call(rbind, cov2(hunter_data, "weeks", "human"))
-  day_hunt_cov2 <- do.call(rbind, cov2(hunter_data, "days", "human"))
+  week_hunt_cov2 <- do.call(rbind, cov2(hunter_data, "weeks", "human", m = 5))
+  day_hunt_cov2 <- do.call(rbind, cov2(hunter_data, "days", "human", m = 5))
   
-  week_hunt_cov3 <- do.call(rbind, cov3(hunter_data, "weeks", "human"))
-  day_hunt_cov3 <- do.call(rbind, cov3(hunter_data, "days", "human"))
+  week_hunt_cov3 <- do.call(rbind, cov3(hunter_data, "weeks", "human", m = 5))
+  day_hunt_cov3 <- do.call(rbind, cov3(hunter_data, "days", "human", m = 5))
   
-  week_hunt_cov4 <- do.call(rbind, cov4(hunter_data, "weeks", "human"))
-  day_hunt_cov4 <- do.call(rbind, cov4(hunter_data, "days", "human"))
+  week_hunt_cov4 <- do.call(rbind, cov4(hunter_data, "weeks", "human", m = 5))
+  day_hunt_cov4 <- do.call(rbind, cov4(hunter_data, "days", "human", m = 5))
   
   
   ## Pearson's Correlations --------------------------------------------------
   #' creating dataframes with the Counts/Durations to be correlated
-  week_hunt_cor_df <- cbind(week_hunt_cov1, week_hunt_cov2$Count, 
+  week_hunt_cor_df <- cbind(week_hunt_cov1, week_hunt_cov2$n_Detections, 
                             week_hunt_cov3$Count, week_hunt_cov4$Duration)
   colnames(week_hunt_cor_df) <- c("CameraLocation", "StartDate", "EndDate", 
-                                  "Cov1", "Cov2", "Cov3", "Cov4") 
-  day_hunt_cor_df <- cbind(day_hunt_cov1, day_hunt_cov2$Count, day_hunt_cov3$Count, 
+                                  "Cov1_n_Images", "Cov2_n_Detections", 
+                                  "Cov3_max_Individuals", "Cov4_Duration") 
+  day_hunt_cor_df <- cbind(day_hunt_cov1, day_hunt_cov2$n_Detections, day_hunt_cov3$Count, 
                            day_hunt_cov4$Duration)
-  colnames(day_hunt_cor_df) <- c("CameraLocation", "Date", "Cov1", "Cov2", "Cov3", 
-                                 "Cov4") 
+  colnames(day_hunt_cor_df) <- c("CameraLocation", "Date", "Cov1_n_Images", 
+                                 "Cov2_n_Detections", "Cov3_max_Individuals", 
+                                 "Cov4_Duration") 
   
   # Weeks
   cor(week_hunt_cor_df[,4:7])
-  #cov1, cov2, cov3 are all highly correlated (r = 0.88 - 0.96)
-  #cov4 is most correlated with cov1 (0.69), much lower with other covs (~0.52)
+  # m = 5 minutes: cov1, cov2, cov3 all highly correlated (r = 0.88 - 0.96)
+  #                cov4 least correlated (r = 0.51 - 0.68)
+  # m = 30 minutes: cov1, cov2, cov3 all highly correlated (r = 0.84 - 0.96)
+  #                 cov4 least correlated (r = 0.46 - 0.65)
   
   # Days
   cor(day_hunt_cor_df[3:6])
-  #cov1, cov2, cov3 are all highly correlated (r = 0.86 - 0.94)
-  #cov4 is most correlated with cov1 (0.67), much lower with other covs (~0.47)
+  # m = 5 minutes: cov1, cov2, cov3 all highly correlated (r = 0.86 - 0.94)
+  #                cov4 least correlated (r = 0.47 - 0.67)
+  # m = 30 minutes: cov1, cov2, cov3 all highly correlated (r = 0.80 - 0.94)
+  #                 cov4 least correlated (r = 0.39 - 0.59)
   
   # RIFLE HUNT --------------------------------------------------------------
   
@@ -84,26 +90,28 @@
   week_rifle_cov1 <-  do.call(rbind, cov1(rifle_data, "weeks", "human"))
   day_rifle_cov1 <- do.call(rbind, cov1(rifle_data, "days", "human"))
   
-  week_rifle_cov2 <- do.call(rbind, cov2(rifle_data, "weeks", "human"))
-  day_rifle_cov2 <- do.call(rbind, cov2(rifle_data, "days", "human"))
+  week_rifle_cov2 <- do.call(rbind, cov2(rifle_data, "weeks", "human", m = 5))
+  day_rifle_cov2 <- do.call(rbind, cov2(rifle_data, "days", "human", m = 5))
   
-  week_rifle_cov3 <- do.call(rbind, cov3(rifle_data, "weeks", "human"))
-  day_rifle_cov3 <- do.call(rbind, cov3(rifle_data, "days", "human"))
+  week_rifle_cov3 <- do.call(rbind, cov3(rifle_data, "weeks", "human", m = 5))
+  day_rifle_cov3 <- do.call(rbind, cov3(rifle_data, "days", "human", m = 5))
   
-  week_rifle_cov4 <- do.call(rbind, cov4(rifle_data, "weeks", "human"))
-  day_rifle_cov4 <- do.call(rbind, cov4(rifle_data, "days", "human"))
+  week_rifle_cov4 <- do.call(rbind, cov4(rifle_data, "weeks", "human", m = 5))
+  day_rifle_cov4 <- do.call(rbind, cov4(rifle_data, "days", "human", m = 5))
   
   
   ## Pearson's Correlations --------------------------------------------------
   # creating dataframes with the Counts/Durations to be correlated
-  week_rifle_cor_df <- cbind(week_rifle_cov1, week_rifle_cov2$Count, 
+  week_rifle_cor_df <- cbind(week_rifle_cov1, week_rifle_cov2$n_Detections, 
                              week_rifle_cov3$Count, week_rifle_cov4$Duration)
   colnames(week_rifle_cor_df) <- c("CameraLocation", "StartDate", "EndDate", 
-                                   "Cov1", "Cov2", "Cov3", "Cov4") 
-  day_rifle_cor_df <- cbind(day_rifle_cov1, day_rifle_cov2$Count, day_rifle_cov3$Count, 
+                                   "Cov1_n_Images", "Cov2_n_Detections", 
+                                   "Cov3_max_Individuals", "Cov4_Duration") 
+  day_rifle_cor_df <- cbind(day_rifle_cov1, day_rifle_cov2$n_Detections, day_rifle_cov3$Count, 
                             day_rifle_cov4$Duration)
-  colnames(day_rifle_cor_df) <- c("CameraLocation", "Date", "Cov1", "Cov2", "Cov3", 
-                                  "Cov4") 
+  colnames(day_rifle_cor_df) <- c("CameraLocation", "Date", "Cov1_n_Images", 
+                                  "Cov2_n_Detections",  "Cov3_max_Individuals", 
+                                  "Cov4_Duration") 
   
   # Weeks
   cor(week_rifle_cor_df[,4:7])
@@ -122,26 +130,28 @@
   week_bow_cov1 <-  do.call(rbind, cov1(bow_data, "weeks", "human"))
   day_bow_cov1 <- do.call(rbind, cov1(bow_data, "days", "human"))
   
-  week_bow_cov2 <- do.call(rbind, cov2(bow_data, "weeks", "human"))
-  day_bow_cov2 <- do.call(rbind, cov2(bow_data, "days", "human"))
+  week_bow_cov2 <- do.call(rbind, cov2(bow_data, "weeks", "human", m = 5))
+  day_bow_cov2 <- do.call(rbind, cov2(bow_data, "days", "human", m = 5))
   
-  week_bow_cov3 <- do.call(rbind, cov3(bow_data, "weeks", "human"))
-  day_bow_cov3 <- do.call(rbind, cov3(bow_data, "days", "human"))
+  week_bow_cov3 <- do.call(rbind, cov3(bow_data, "weeks", "human", m = 5))
+  day_bow_cov3 <- do.call(rbind, cov3(bow_data, "days", "human", m = 5))
   
-  week_bow_cov4 <- do.call(rbind, cov4(bow_data, "weeks", "human"))
-  day_bow_cov4 <- do.call(rbind, cov4(bow_data, "days", "human"))
+  week_bow_cov4 <- do.call(rbind, cov4(bow_data, "weeks", "human", m = 5))
+  day_bow_cov4 <- do.call(rbind, cov4(bow_data, "days", "human", m = 5))
   
   
   ## Pearson's Correlations --------------------------------------------------
   # creating dataframes with the Counts/Durations to be correlated
-  week_bow_cor_df <- cbind(week_bow_cov1, week_bow_cov2$Count, week_bow_cov3$Count, 
+  week_bow_cor_df <- cbind(week_bow_cov1, week_bow_cov2$n_Detections, week_bow_cov3$Count, 
                            week_bow_cov4$Duration)
   colnames(week_bow_cor_df) <- c("CameraLocation", "StartDate", "EndDate", 
-                                 "Cov1", "Cov2", "Cov3", "Cov4") 
-  day_bow_cor_df <- cbind(day_bow_cov1, day_bow_cov2$Count, day_bow_cov3$Count, 
+                                 "Cov1_n_Images", "Cov2_n_Detections", 
+                                 "Cov3_max_Individuals", "Cov4_Duration") 
+  day_bow_cor_df <- cbind(day_bow_cov1, day_bow_cov2$n_Detections, day_bow_cov3$Count, 
                           day_bow_cov4$Duration)
-  colnames(day_bow_cor_df) <- c("CameraLocation", "Date", "Cov1", "Cov2", "Cov3", 
-                                "Cov4") 
+  colnames(day_bow_cor_df) <- c("CameraLocation", "Date", "Cov1_n_Images", 
+                                "Cov2_n_Detections", "Cov3_max_Individuals", 
+                                "Cov4_Duration") 
   
   # Weeks
   cor(week_bow_cor_df[,4:7]) 
@@ -159,26 +169,28 @@
   week_vehicle_cov1 <-  do.call(rbind, cov1(vehicle_data, "weeks", "human"))
   day_vehicle_cov1 <- do.call(rbind, cov1(vehicle_data, "days", "human"))
   
-  week_vehicle_cov2 <- do.call(rbind, cov2(vehicle_data, "weeks", "human"))
-  day_vehicle_cov2 <- do.call(rbind, cov2(vehicle_data, "days", "human"))
+  week_vehicle_cov2 <- do.call(rbind, cov2(vehicle_data, "weeks", "human", m = 5))
+  day_vehicle_cov2 <- do.call(rbind, cov2(vehicle_data, "days", "human", m = 5))
   
-  week_vehicle_cov3 <- do.call(rbind, cov3(vehicle_data, "weeks", "human"))
-  day_vehicle_cov3 <- do.call(rbind, cov3(vehicle_data, "days", "human"))
+  week_vehicle_cov3 <- do.call(rbind, cov3(vehicle_data, "weeks", "human", m = 5))
+  day_vehicle_cov3 <- do.call(rbind, cov3(vehicle_data, "days", "human", m = 5))
   
-  week_vehicle_cov4 <- do.call(rbind, cov4(vehicle_data, "weeks", "human"))
-  day_vehicle_cov4 <- do.call(rbind, cov4(vehicle_data, "days", "human"))
+  week_vehicle_cov4 <- do.call(rbind, cov4(vehicle_data, "weeks", "human", m = 5))
+  day_vehicle_cov4 <- do.call(rbind, cov4(vehicle_data, "days", "human", m = 5))
   
   
   ## Pearson's Correlations --------------------------------------------------
   # creating dataframes with the Counts/Durations to be correlated
-  week_vehicle_cor_df <- cbind(week_vehicle_cov1, week_vehicle_cov2$Count, 
+  week_vehicle_cor_df <- cbind(week_vehicle_cov1, week_vehicle_cov2$n_Detections, 
                                week_vehicle_cov3$Count, week_vehicle_cov4$Duration)
   colnames(week_vehicle_cor_df) <- c("CameraLocation", "StartDate", "EndDate", 
-                                     "Cov1", "Cov2", "Cov3", "Cov4") 
-  day_vehicle_cor_df <- cbind(day_vehicle_cov1, day_vehicle_cov2$Count, 
+                                     "Cov1_n_Images", "Cov2_n_Detections", 
+                                     "Cov3_max_Individuals", "Cov4_Duration") 
+  day_vehicle_cor_df <- cbind(day_vehicle_cov1, day_vehicle_cov2$n_Detections, 
                               day_vehicle_cov3$Count, day_vehicle_cov4$Duration)
-  colnames(day_vehicle_cor_df) <- c("CameraLocation", "Date", "Cov1", "Cov2", 
-                                    "Cov3", "Cov4") 
+  colnames(day_vehicle_cor_df) <- c("CameraLocation", "Date", "Cov1_n_Images", 
+                                    "Cov2_n_Detections", "Cov3_max_Individuals", 
+                                    "Cov4_Duration") 
   
   # Weeks
   cor(week_vehicle_cor_df[,4:7])
@@ -196,26 +208,28 @@
   week_car_cov1 <-  do.call(rbind, cov1(car_data, "weeks", "human"))
   day_car_cov1 <- do.call(rbind, cov1(car_data, "days", "human"))
   
-  week_car_cov2 <- do.call(rbind, cov2(car_data, "weeks", "human"))
-  day_car_cov2 <- do.call(rbind, cov2(car_data, "days", "human"))
+  week_car_cov2 <- do.call(rbind, cov2(car_data, "weeks", "human", m = 5))
+  day_car_cov2 <- do.call(rbind, cov2(car_data, "days", "human", m = 5))
   
-  week_car_cov3 <- do.call(rbind, cov3(car_data, "weeks", "human"))
-  day_car_cov3 <- do.call(rbind, cov3(car_data, "days", "human"))
+  week_car_cov3 <- do.call(rbind, cov3(car_data, "weeks", "human", m = 5))
+  day_car_cov3 <- do.call(rbind, cov3(car_data, "days", "human", m = 5))
   
-  week_car_cov4 <- do.call(rbind, cov4(car_data, "weeks", "human"))
-  day_car_cov4 <- do.call(rbind, cov4(car_data, "days", "human"))
+  week_car_cov4 <- do.call(rbind, cov4(car_data, "weeks", "human", m = 5))
+  day_car_cov4 <- do.call(rbind, cov4(car_data, "days", "human", m = 5))
   
   
   ## Pearson's Correlations --------------------------------------------------
   # creating dataframes with the Counts/Durations to be correlated
-  week_car_cor_df <- cbind(week_car_cov1, week_car_cov2$Count, week_car_cov3$Count, 
+  week_car_cor_df <- cbind(week_car_cov1, week_car_cov2$n_Detections, week_car_cov3$Count, 
                            week_car_cov4$Duration)
   colnames(week_car_cor_df) <- c("CameraLocation", "StartDate", "EndDate", 
-                                 "Cov1", "Cov2", "Cov3", "Cov4") 
-  day_car_cor_df <- cbind(day_car_cov1, day_car_cov2$Count, day_car_cov3$Count, 
+                                 "Cov1_n_Images", "Cov2_n_Detections", 
+                                 "Cov3_max_Individuals", "Cov4_Duration") 
+  day_car_cor_df <- cbind(day_car_cov1, day_car_cov2$n_Detections, day_car_cov3$Count, 
                           day_car_cov4$Duration)
-  colnames(day_car_cor_df) <- c("CameraLocation", "Date", "Cov1", "Cov2", "Cov3", 
-                                "Cov4") 
+  colnames(day_car_cor_df) <- c("CameraLocation", "Date", "Cov1_n_Images", 
+                                "Cov2_n_Detections", "Cov3_max_Individuals", 
+                                "Cov4_Duration") 
   
   # Weeks
   cor(week_car_cor_df[,4:7])
@@ -232,26 +246,28 @@
   week_atv_cov1 <-  do.call(rbind, cov1(atv_data, "weeks", "human"))
   day_atv_cov1 <- do.call(rbind, cov1(atv_data, "days", "human"))
   
-  week_atv_cov2 <- do.call(rbind, cov2(atv_data, "weeks", "human"))
-  day_atv_cov2 <- do.call(rbind, cov2(atv_data, "days", "human"))
+  week_atv_cov2 <- do.call(rbind, cov2(atv_data, "weeks", "human", m = 5))
+  day_atv_cov2 <- do.call(rbind, cov2(atv_data, "days", "human", m = 5))
   
-  week_atv_cov3 <- do.call(rbind, cov3(atv_data, "weeks", "human"))
-  day_atv_cov3 <- do.call(rbind, cov3(atv_data, "days", "human"))
+  week_atv_cov3 <- do.call(rbind, cov3(atv_data, "weeks", "human", m = 5))
+  day_atv_cov3 <- do.call(rbind, cov3(atv_data, "days", "human", m = 5))
   
-  week_atv_cov4 <- do.call(rbind, cov4(atv_data, "weeks", "human"))
-  day_atv_cov4 <- do.call(rbind, cov4(atv_data, "days", "human"))
+  week_atv_cov4 <- do.call(rbind, cov4(atv_data, "weeks", "human", m = 5))
+  day_atv_cov4 <- do.call(rbind, cov4(atv_data, "days", "human", m = 5))
   
   
   ## Pearson's Correlations --------------------------------------------------
   # creating dataframes with the Counts/Durations to be correlated
-  week_atv_cor_df <- cbind(week_atv_cov1, week_atv_cov2$Count, week_atv_cov3$Count, 
+  week_atv_cor_df <- cbind(week_atv_cov1, week_atv_cov2$n_Detections, week_atv_cov3$Count, 
                            week_atv_cov4$Duration)
   colnames(week_atv_cor_df) <- c("CameraLocation", "StartDate", "EndDate", 
-                                 "Cov1", "Cov2", "Cov3", "Cov4") 
-  day_atv_cor_df <- cbind(day_atv_cov1, day_atv_cov2$Count, day_atv_cov3$Count, 
+                                 "Cov1_n_Images", "Cov2_n_Detections", 
+                                 "Cov3_max_Individuals", "Cov4_Duration") 
+  day_atv_cor_df <- cbind(day_atv_cov1, day_atv_cov2$n_Detections, day_atv_cov3$Count, 
                           day_atv_cov4$Duration)
-  colnames(day_atv_cor_df) <- c("CameraLocation", "Date", "Cov1", "Cov2", "Cov3", 
-                                "Cov4") 
+  colnames(day_atv_cor_df) <- c("CameraLocation", "Date", "Cov1_n_Images", 
+                                "Cov2_n_Detections", "Cov3_max_Individuals", 
+                                "Cov4_Duration") 
   
   # Weeks
   cor(week_atv_cor_df[,4:7])
@@ -272,26 +288,28 @@
   week_hike_cov1 <-  do.call(rbind, cov1(hike_data, "weeks", "human"))
   day_hike_cov1 <- do.call(rbind, cov1(hike_data, "days", "human"))
   
-  week_hike_cov2 <- do.call(rbind, cov2(hike_data, "weeks", "human"))
-  day_hike_cov2 <- do.call(rbind, cov2(hike_data, "days", "human"))
+  week_hike_cov2 <- do.call(rbind, cov2(hike_data, "weeks", "human", m = 5))
+  day_hike_cov2 <- do.call(rbind, cov2(hike_data, "days", "human", m = 5))
   
-  week_hike_cov3 <- do.call(rbind, cov3(hike_data, "weeks", "human"))
-  day_hike_cov3 <- do.call(rbind, cov3(hike_data, "days", "human"))
+  week_hike_cov3 <- do.call(rbind, cov3(hike_data, "weeks", "human", m = 5))
+  day_hike_cov3 <- do.call(rbind, cov3(hike_data, "days", "human", m = 5))
   
-  week_hike_cov4 <- do.call(rbind, cov4(hike_data, "weeks", "human"))
-  day_hike_cov4 <- do.call(rbind, cov4(hike_data, "days", "human"))
+  week_hike_cov4 <- do.call(rbind, cov4(hike_data, "weeks", "human", m = 5))
+  day_hike_cov4 <- do.call(rbind, cov4(hike_data, "days", "human", m = 5))
   
   
   ## Pearson's Correlations --------------------------------------------------
   # creating dataframes with the Counts/Durations to be correlated
-  week_hike_cor_df <- cbind(week_hike_cov1, week_hike_cov2$Count, week_hike_cov3$Count, 
+  week_hike_cor_df <- cbind(week_hike_cov1, week_hike_cov2$n_Detections, week_hike_cov3$Count, 
                             week_hike_cov4$Duration)
   colnames(week_hike_cor_df) <- c("CameraLocation", "StartDate", "EndDate", 
-                                  "Cov1", "Cov2", "Cov3", "Cov4") 
-  day_hike_cor_df <- cbind(day_hike_cov1, day_hike_cov2$Count, day_hike_cov3$Count, 
+                                  "Cov1_n_Images", "Cov2_n_Detections", 
+                                  "Cov3_max_Individuals", "Cov4_Duration") 
+  day_hike_cor_df <- cbind(day_hike_cov1, day_hike_cov2$n_Detections, day_hike_cov3$Count, 
                            day_hike_cov4$Duration)
-  colnames(day_hike_cor_df) <- c("CameraLocation", "Date", "Cov1", "Cov2", "Cov3", 
-                                 "Cov4") 
+  colnames(day_hike_cor_df) <- c("CameraLocation", "Date", "Cov1_n_Images", 
+                                 "Cov2_n_Detections", "Cov3_max_Individuals", 
+                                 "Cov4_Duration") 
   
   # Weeks
   cor(week_hike_cor_df[,4:7])
